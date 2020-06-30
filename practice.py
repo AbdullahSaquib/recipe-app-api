96f2b764a10ddf7432b14daf455bd2c8d7a73725
AbdullahSaquib <abdullahsaquib@gmail.com>
2020-06-30 18:34:05 +0530

    Added image field to recipe model.

--- practice.py
+++ practice.py
@@ -1,81 +1,103 @@
-class BinaryTree:
-    def __init__(self, key):
-        self.key = key
-        self.left_child = None
-        self.right_child = None
+class TreeNode:
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        self.key
+        self.payload = val
+        self.left_child = left
+        self.right_child = right
+        self.parent = parent
 
-    def insert_left(self, key):
-        if self.left_child:
-            new_node = BinaryTree(key)
-            new_node.left_child = self.left_child
-            self.left_child = new_node
-        else:
-            self.left_child = BinaryTree(key)
+    def has_left_child(self):
         return self.left_child
 
-    def insert_right(self, key):
-        if self.right_child:
-            new_node = BinaryTree(key)
-            new_node.right_child = self.right_child
-            self.right_child = new_node
-        else:
-            self.right_child = BinaryTree(key)
+    def has_right_child(self):
         return self.right_child
 
-    def get_right(self):
-        return self.right_child
+    def is_left_child(self):
+        return self.parent and (self.parent.left_child == self)
 
-    def get_left(self):
-        return self.left_child
+    def is_right_child(self):
+        return self.parent and (self.parent.right_child == self)
 
-    def set_key(self, key):
+    def is_root(self):
+        return not self.parent
+
+    def is_leaf(self):
+        return not (self.left_child and self.right_child)
+
+    def has_any_children(self):
+        return self.left_child or self.right_child
+
+    def has_both_children(self):
+        return self.left_child and self.right_child
+
+    def replace_node_data(self, key, value, lc, rc):
         self.key = key
-
-    def get_key(self):
-        return self.key
-
-    def preorder(self):
-        print(self.key)
-        if self.left_child:
-            self.left_child.preorder()
-        if self.right_child:
-            self.right_child.preorder()
-
-    def inorder(self):
-        if self.left_child:
-            self.left_child.inorder()
-        print(self.key)
-        if self.right_child:
-            self.right_child.inorder()
-
-    def postorder(self):
-        if self.left_child:
-            self.left_child.inorder()
-        if self.right_child:
-            self.right_child.inorder()
-        print(self.key)
+        self.payload = value
+        self.left_child = lc
+        self.right_child = rc
+        if self.has_left_child():
+            self.left_child.parent = self
+        if self.has_right_child():
+            self.right_child.parent = self
 
 
-a = BinaryTree('a')
-b = a.insert_left('b')
-c = a.insert_right('c')
-d = b.insert_left('d')
-e = b.insert_right('e')
-f = c.insert_left('f')
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
 
-# b = a.get_left()
-# c = a.get_right()
-# d = b.get_left()
-# e = b.get_right()
-# f = c.get_left()
+    def length(self):
+        return self.size
 
-# print(a.key)
-# print(b.key)
-# print(c.key)
-# print(d.key)
-# print(e.key)
-# print(f.key)
+    def __len__(self):
+        return self.size
 
-a.preorder()
-a.inorder()
-a.postorder()
+    def put(self, key, value):
+        if self.root:
+            self._put(key, value, self.root)
+        else:
+            self.root = TreeNode(key, value)
+        self.size += 1
+
+    def _put(self, key, value, current_node):
+        if key < current_node.key:
+            if current_node.has_left_child():
+                self._put(key, value, current_node.left_child)
+            else:
+                current_node.left_child = TreeNode(key, value)
+        else:
+            if current_node.has_right_child():
+                self._put(key, value, current_node.right_child)
+            else:
+                current_node.right_child = TreeNode(key, value)
+
+    def __setitem__(self, k, v):
+        self.put(k, v)
+
+    def get(self, key):
+        if self.root:
+            result = self._get(key, self.root)
+            if result:
+                return result.payload
+            else:
+                return None
+        else:
+            return None
+
+    def _get(self, key, current_node):
+        if current_node == None:
+            return None
+        elif current_node.key == key:
+            return current_node
+        elif key < current_node.key:
+            return self._get(key, current_node.left_child)
+        else:
+            return self._get(key, current_node.right_child)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __contains__(self, key):
+        if self._get(key, self.root):
+            return True
+        return False
